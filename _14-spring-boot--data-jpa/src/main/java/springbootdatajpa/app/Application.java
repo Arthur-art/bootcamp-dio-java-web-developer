3870d89121b92264221a90aa5e24176b681ef49a
arthur-art <arthurteixeira.guts@gmail.com>
2022-11-01 18:29:48 -0300

    Iniciando um projeto com spring jpa

---
+++ _14-spring-boot--data-jpa/src/main/java/springbootdatajpa/app/Application.java
@@ -0,0 +1,13 @@
+package springbootdatajpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
